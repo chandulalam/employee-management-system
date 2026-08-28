19091dbeadb4b510f6cc4cb3deca59265f98ad0d
chandu <chandulalam207@gmail.com>
2026-08-29 01:40:51 +0530

    feat: add employee mapper

---
+++ src/main/java/mapper/EmployeeMapper.java
@@ -0,0 +1,37 @@
+package mapper;
+
+import com.emp.dto.EmployeeRequestDTO;
+import com.emp.dto.EmployeeResponseDTO;
+import com.emp.model.Employee;
+
+public class EmployeeMapper {
+	
+	public Employee employeeDtoToEmployee(EmployeeRequestDTO employeeRequestDTO) {
+		Employee employee = new Employee();
+		employee.setFirstName(employeeRequestDTO.getFirstName());
+		employee.setLastName(employeeRequestDTO.getLastName());
+		employee.setEmail(employeeRequestDTO.getEmail());
+		employee.setPhone(employeeRequestDTO.getPhone());
+		employee.setSalary(employeeRequestDTO.getSalary());
+		employee.setJoiningDate(employeeRequestDTO.getJoiningDate());
+		employee.setStatus(employeeRequestDTO.getStatus());
+
+		return employee;		
+	}
+	
+	public EmployeeResponseDTO employeeToEmployeeResponse(Employee emp) {
+		EmployeeResponseDTO response=new EmployeeResponseDTO();
+		response.setId(emp.getId());
+		response.setFirstName(emp.getFirstName());
+		response.setLastName(emp.getLastName());
+		response.setEmail(emp.getEmail());
+		response.setPhone(emp.getPhone());
+		response.setSalary(emp.getSalary());
+		response.setJoiningDate(emp.getJoiningDate());
+		response.setStatus(emp.getStatus());
+		response.setDepartmentId(emp.getDepartment().getId());
+		
+		return response;
+	}
+	
+}
