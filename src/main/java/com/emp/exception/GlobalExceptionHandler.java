286a168c1a92cf653f0f0231e1760efac7d25e23
chandu <chandulalam207@gmail.com>
2026-08-30 12:09:34 +0530

    feat : add handling validation exception

--- src/main/java/com/emp/exception/GlobalExceptionHandler.java
+++ src/main/java/com/emp/exception/GlobalExceptionHandler.java
@@ -1,9 +1,11 @@
 package com.emp.exception;
 
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -54,4 +56,25 @@ public class GlobalExceptionHandler {
 				.status(HttpStatus.NOT_FOUND)
 				.body(error);
 	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ErrorResponseDTO> handleValidationException(MethodArgumentNotValidException methodArgumentNotValidException){
+		String message = methodArgumentNotValidException.getBindingResult()
+	            .getFieldErrors()
+	            .stream()
+	            .map(error -> error.getField() + ": " + error.getDefaultMessage())
+	            .collect(Collectors.joining(", "));
+		
+		 ErrorResponseDTO error = new ErrorResponseDTO(
+		            HttpStatus.BAD_REQUEST.value(),
+		            message,
+		            LocalDateTime.now()
+		    );
+
+		    return ResponseEntity
+		            .status(HttpStatus.BAD_REQUEST)
+		            .body(error);
+		
+	}
+	
 }
